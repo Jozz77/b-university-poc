2c4bf72c337553a291f5581fc0a1ec85e38eee94
Jonathan Mmadu <mmadujonathan@gmail.com>
2026-03-14 22:39:27 +0100

    fix: Correctly place `output` and `images` configuration at the top level of `next.config.ts`.

--- next.config.ts
+++ next.config.ts
@@ -3,14 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  output: 'export', // Tells Next.js to produce a static site
+  images: {
+    unoptimized: true, // Required because GitHub Pages doesn't support the default Image Optimization
+  },
   async headers() {
     return [
       {
         source: '/(.*)',
-        output: 'export', // Tells Next.js to produce a static site
-        images: {
-          unoptimized: true, // Required because GitHub Pages doesn't support the default Image Optimization
-        },
         headers: [
           {
             key: 'Cross-Origin-Embedder-Policy',
